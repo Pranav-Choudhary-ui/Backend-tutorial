167c41bf904bc56920af4fa89b56c804523be7eb
Pranav Choudhary <pranavsingh4703@gmail.com>
2026-02-09 22:22:50 +0530

    added some more files

---
+++ 01_import-packages-from-npm/server.js
@@ -0,0 +1,11 @@
+const cat = require('cat-me')
+
+console.log(cat())
+
+
+// node module contain all the files of the package
+// package.json contains the list of packages my code is using
+// package-lock.json contains the list of all the dependencies -> our code + packages imported
+
+// to start a node.js app professionally
+// in terminal -> npm init -y
