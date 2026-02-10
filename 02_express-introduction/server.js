d08af1222efac2daf18debefc97b3f5ddf8f60cc
Pranav Choudhary <pranavsingh4703@gmail.com>
2026-02-10 10:31:26 +0530

    express intro

---
+++ 02_express-introduction/server.js
@@ -0,0 +1,15 @@
+const express = require('express')      // loads the Express module
+const app = express()       // creating the server instance
+
+
+// req contains all the data coming from front-end
+app.get('/', (req, res) => {
+    res.send('Hello World!!')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About Page')
+})
+
+
+app.listen(3000)        // starts an server
