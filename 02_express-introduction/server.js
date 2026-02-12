87c3973062df9a1f562e9e4959278f70bfe97317
Pranav Choudhary <pranavsingh4703@gmail.com>
2026-02-12 12:28:23 +0530

    update

--- 02_express-introduction/server.js
+++ 02_express-introduction/server.js
@@ -1,7 +1,7 @@
 const express = require('express')      // loads the Express module
 const app = express()       // creating the server instance
 
-
+// express provides methods to handle different HTTP requests (GET, POST, PUT, DELETE, etc.)
 // req contains all the data coming from front-end
 app.get('/', (req, res) => {
     res.send('Hello World!!')
